0f2fa8384385818e11c95f8e76cb7912d86f59fe
Luciano Graziani <lgraziani2712@gmail.com>
2026-07-23 19:02:38 -0300

    add tileset resource with terrains

--- GMTK.profile
+++ GMTK.profile
@@ -1,14 +1,21 @@
 {
 	"disabled_classes": [
+		"Curve3D",
+		"MultiplayerSpawner",
+		"MultiplayerSynchronizer",
 		"NavigationAgent3D",
 		"Node3D",
+		"ParallaxBackground",
+		"ParallaxLayer",
 		"Shape3D",
+		"Texture3D",
 		"TileMap",
 		"World3D"
 	],
 	"disabled_editors": [],
 	"disabled_features": [
-		"3d"
+		"3d",
+		"node_dock"
 	],
 	"disabled_properties": [],
 	"type": "feature_profile"
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,30 +1,32 @@
 extends CharacterBody2D
 
-@export var JUMP_HEIGHT := -450.0
-@export var GRAVITY := 20.5
-@export var JUMP_HOLD := 0.5
-@export var FAST_FALL_GRAVITY := 600.0
+@export var jump_height: float = -450.0
+@export var gravity: float = 20.5
+@export var jump_hold: float = 0.5
+@export var fast_fall_gravity: float = 600.0
 
-@export var MAX_SPEED := 400.0
-@export var ACCELERATION := 52.5
-@export var FRICTION := 12.5
+@export var max_speed: float = 400.0
+@export var acceleration: float = 52.5
+@export var friction: float = 12.5
 
-@export var AIR_ACCELERATION := 10.0
+@export var air_acceleration: float = 10.0
+
 
 func _physics_process(delta: float) -> void:
-	var hor := Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	var acc := ACCELERATION if is_on_floor() else AIR_ACCELERATION
-	var velocity_weight := delta * (acc if hor else FRICTION)
-	velocity.x = lerp(velocity.x, hor * MAX_SPEED, velocity_weight)
-	
+	var hor: float = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	var acc: float = acceleration if is_on_floor() else air_acceleration
+	var velocity_weight: float = delta * (acc if hor else friction)
+
+	velocity.x = lerp(velocity.x, hor * max_speed, velocity_weight)
+
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
-			velocity.y = JUMP_HEIGHT
+			velocity.y = jump_height
 	elif velocity.y < 0.0:
 		if Input.is_action_just_released("ui_up"):
-			velocity.y *= JUMP_HOLD
+			velocity.y *= jump_hold
 		elif Input.is_action_pressed("ui_down"):
-			velocity.y += FAST_FALL_GRAVITY * delta
-	
-	velocity.y += GRAVITY
+			velocity.y += fast_fall_gravity * delta
+
+	velocity.y += gravity
 	move_and_slide()
