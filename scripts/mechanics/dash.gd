ea43511a10d838cb95801284fbc75475514638e2
Matias Ricarte <mricarte@itba.edu.ar>
2026-07-23 23:24:38 -0300

    fix typo missed during stash

--- scripts/mechanics/dash.gd
+++ scripts/mechanics/dash.gd
@@ -11,20 +11,16 @@ var _dir: Vector2 = Vector2.ZERO
 
 @onready var timer: Timer = $Timer
 
-
 func _ready() -> void:
 	timer.wait_time = dash_cooldown
-
 
 func can_activate() -> bool:
 	if _cooling_down:
 		return false
 	return Input.is_action_just_pressed("dash")
 
-
 func on_enter() -> void:
-	var dir: Vector2 = player.read_input_direction()
-
+	var dir = player.read_input_direction()
 	_dir = dir if dir != Vector2.ZERO else player.last_facing
 
 	_time_left = dash_duration
@@ -32,12 +28,10 @@ func on_enter() -> void:
 	timer.start()
 	player.velocity = _dir * dash_speed
 
-
 func on_physics_process(delta: float) -> bool:
 	_time_left -= delta
 	player.velocity = _dir * dash_speed
 	return _time_left > 0.0 # we dash until the time is up
-
 
 func _on_timer_timeout() -> void:
 	_cooling_down = false
