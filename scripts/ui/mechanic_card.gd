83b2ff9d4789427ea8f3ca687c2e07d1bf7b1f4d
Luciano Graziani <lgraziani2712@gmail.com>
2026-07-26 12:36:20 -0300

    connect mechanic cards content

--- scripts/ui/mechanic_card.gd
+++ scripts/ui/mechanic_card.gd
@@ -2,6 +2,19 @@ extends Button
 class_name MechanicCard
 
 @export var type: Mechanic.Type
+@export var image: Texture
+@export var title: String
+@export var description: String
+
+@onready var _skill: TextureRect = $MarginContainer/VBoxContainer/Skill
+@onready var _title: Label = $MarginContainer/VBoxContainer/Title
+@onready var _description: RichTextLabel = $MarginContainer/VBoxContainer/Description
+
+
+func _ready() -> void:
+	_skill.texture = image
+	_title.text = title
+	_description.text = description
 
 
 func _on_pressed() -> void:
